a525886fcea849d7ad2912baf6436663e70e1165
Richard Phillips <ccsrip@bath.ac.uk>
2016-03-30 18:26:40 +0100

    Used easing functions for servo movement and added copyright header

--- Signalpost/Signalpost.ino
+++ Signalpost/Signalpost.ino
@@ -6,6 +6,39 @@
  * GL5 Mainline Association, Signal Project (for Shildon Museum display March 2016)
  *
  * 2016
+ *
+ * https://github.com/jesusgollonet/ofpennereasing/tree/master/PennerEasing
+
+TERMS OF USE - EASING EQUATIONS
+Open source under the BSD License http://www.opensource.org/licenses/bsd-license.php
+
+Copyright © 2001 Robert Penner
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+Neither the name of the author nor the names of contributors may be used to
+endorse or promote products derived from this software without specific
+prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
  */
 #include <SimpleModbusSlave.h>
 #include <Servo.h>
@@ -23,7 +56,6 @@ const int servoEnduS = 2301;  // end uS   of servo range (=180 dgrees), only cha
 // Do not drive servo's beyond this angle, set with care.
 const unsigned int DRIVELIMIT = 45;
 
-
 // Default angle to send signals to for "OFF"
 const unsigned int DRIVEANGLE = 45;
 
@@ -31,7 +63,16 @@ const unsigned int NUMARMS = 4;
 
 unsigned char sID = 1;  // Initial Slave ID before setting with DIP switch
 
+const int upTransitionTimemS = 1700;    // time allowed in milliseconds for off/up transition
+const int downTransitionTimemS = 1500;  // time allowed in milliseconds for down/on transition
+// end
 
+// define "EasingFunc" callback function
+// you can make your own of these to use
+// t: current time, b: beginning value, c: change in value, d: duration
+// t and d can be in frames or seconds/milliseconds
+//
+float (*EasingFunc)(float t, float b, float c, int d);
 
 // Map pins in array later from integers
 static const uint8_t analog_pins[] = {A0, A1, A2, A3, A4, A5};
@@ -140,6 +181,11 @@ void loop() {
       j += 2;
     }
 
+
+    // manual mode, if on go to driveangle, write back to read register
+    // if off, if request register is still high, stay high
+    // if off, if request register is low, set low
+
     // manual override
     if (digitalRead(i + j + 3) == LOW) {
       AUTOMODE = false;
@@ -151,8 +197,6 @@ void loop() {
       AUTOMODE = true;
     }
 
-
-
 #ifdef DEBUG
     Serial.println("-------------------");
     Serial.println("-------------------");
@@ -163,8 +207,33 @@ void loop() {
     Serial.print(" REQUEST (READ) Register value: ");
     Serial.println(registers[reqregidx]);
 #endif
-    int easingcurve = 1;
-    int readpos = moveArm(i, easingcurve, registers[reqregidx], transitionTimemS);
+
+    // if ON upTransitionTimemS, func select 1
+    // if ON downTransitionTimemS, func select 2
+
+    // default ON down
+    int easingcurve = 2;
+    transitionTimemS = downTransitionTimemS;
+    // clear (OFF) up
+    if (registers[i] > 0) {
+      easingcurve = 1;
+      transitionTimemS = upTransitionTimemS;
+    }
+
+
+    // only move if there is a change required
+    if (registers[reqregidx] != registers[i]) {
+      int readpos = moveArm(i, easingcurve, registers[reqregidx], transitionTimemS);
+
+      if (AUTOMODE) {
+        registers[i] = readpos;
+      }
+    }
+
+    if (registers[i] != registers[reqregidx] && AUTOMODE == false) {
+      // restore previous value
+      registers[reqregidx] = registers[i];
+    }
 
 #ifdef DEBUG
     Serial.print(i);
@@ -175,14 +244,6 @@ void loop() {
 #endif
 
 
-    if (registers[i] != registers[reqregidx] && AUTOMODE == false) {
-      // restore previous value
-      registers[reqregidx] = registers[i];
-    }
-
-    if (AUTOMODE) {
-      registers[i] = readpos;
-    }
 
   }
 
@@ -195,6 +256,8 @@ void loop() {
 */
 int moveArm(int armI, int armfuncselect, int destDegrees, int transitionTimemS) {
   int posnuS = 1500;          // current position of servo in microSeconds
+  unsigned long currentMillis = 0;
+
   const unsigned int fromLow = 0;
   const unsigned int fromHigh = 180;
   int toLow = servoStartuS;
@@ -209,37 +272,123 @@ int moveArm(int armI, int armfuncselect, int destDegrees, int transitionTimemS) 
   unsigned int DRIVELIMITuS = map(DRIVELIMIT, fromLow, fromHigh, toLow, toHigh);
   unsigned int destuS = map(destDegrees, fromLow, fromHigh, toLow, toHigh);
 
+  switch (armfuncselect) {
+    case 1:
+      EasingFunc = easeOutBounce;
+      break;
+    case 2:
+      EasingFunc = easeOutElastic;
+      break;
+    default:
+      EasingFunc = easeNone;
+  }
 
-  posnuS = destuS;
+  float startuS = arm[armI].readMicroseconds();
+
+  // drive limit here before calculating destination
+
   // DO NOT go beyond absolute drive limit.
   if (armI % 2 == 1) {
-    posnuS = (posnuS > DRIVELIMITuS) ? posnuS : DRIVELIMITuS;
+    destuS = (destuS > DRIVELIMITuS) ? destuS : DRIVELIMITuS;
   }
   else {
-    posnuS = (posnuS < DRIVELIMITuS) ? posnuS : DRIVELIMITuS;
+    destuS = (destuS < DRIVELIMITuS) ? destuS : DRIVELIMITuS;
   }
+  float amountofchange = destuS - startuS;
+
+  currentMillis = millis();
+  unsigned long lastmodbusupdate, startloopMillis = currentMillis;
+
+  // re-attach arm
+  // arm[armNum].attach((armI += (armI > 2) ? 5 : 7), servoStartuS, servoEnduS);  // 5,6 - 9 and 10
+  while (currentMillis - startloopMillis < transitionTimemS) {
+    posnuS = (int)EasingFunc((currentMillis - startloopMillis), startuS, amountofchange, transitionTimemS);
+
+    if ((millis() - lastmodbusupdate) > 500) {
+      modbus_update();
+      lastmodbusupdate =  millis();
+    }
+
 
 #ifdef DEBUG
-  Serial.print(armI);
-  Serial.print(" Drive to uS Position: ");
-  Serial.println(posnuS);
-  Serial.print(" Drive limit uS: ");
-  Serial.println(DRIVELIMITuS);
+    Serial.print(armI);
+    Serial.print(" Drive to uS Position: ");
+    Serial.println(posnuS);
+    Serial.print(" Drive limit uS: ");
+    Serial.println(DRIVELIMITuS);
+    Serial.print(" Current Mill Seconds: (");
+    Serial.println(currentMillis);
+    Serial.println("-------------------");
+    // allow the serial line time to catch up
+    delay(250);
 #endif
-
-
-
-
-  // attach
-  //arm[armI].attach((armI += (armI > 2) ? 5 : 7), servoStartuS, servoEnduS); // 5,6 - 9 and 10
-  arm[armI].writeMicroseconds(posnuS);              // tell servo to go to position in variable 'pos'
+    arm[armI].writeMicroseconds(posnuS);              // tell servo to go to position in variable 'pos'
+    currentMillis = millis();
+  }
   // detach to stop jitter
-  //arm[armI].detach();
+  // arm[armI].detach();
 
-  //return map(arm[armI].readMicroseconds(), toLow, toHigh, fromLow, fromHigh);
   // return degrees,map microseconds to degreees
   return map(arm[armI].readMicroseconds(),  toLow, toHigh, fromLow, fromHigh);
 }
+
+
+/** No easing applied, straight move
+
+*/
+inline float easeNone(float t, float b , float c, int d) {
+  return c * t / d + b;
+}
+
+
+/**
+   Easing function to simulate bounce
+   float t elapsed time (same units as for d ie: milliseconds or seconds)
+   float b initial value
+   float c amount of change (the final value minus the starting/initial value)
+   int d duration
+   Returns: float
+*/
+inline float easeOutBounce(float t, float b, float c, int d) {
+  if ((t /= d) < (1 / 2.75)) {
+    return c * (7.5625 * t * t) + b;
+  } else if (t < (2 / 2.75)) {
+    t -= 1.5 / 2.75;
+    return c * (7.5625 * t * t + 0.75) + b;
+  } else if (t < (2.5 / 2.75)) {
+    t -= 2.25 / 2.75;
+    return c * (7.5625 * t * t + 0.9375) + b;
+  } else {
+    t -= 2.625 / 2.75;
+    return c * (7.5625 * t * t + 0.984375) + b;
+  }
+}
+
+/**
+   Easing function to simulate lift up and fall back
+   !!! NOTE, will drive OVER/past endpoint and back !!!
+   float t elapsed time (same units as for d ie: milliseconds or seconds)
+   float b initial value
+   float c amount of change (the final value minus the starting/initial value)
+   int d duration
+   Returns: float
+*/
+inline float easeOutBack(float t, float b , float c, int d) {
+  float s = 1.70158f;
+  //return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+  t = t / d - 1;
+  return c * (t * t * ((s + 1) * t + s) + 1) + b;
+}
+
+
+inline float easeOutElastic(float t, float b , float c, int d) {
+  if (t == 0) return b;  if ((t /= d) == 1) return b + c;
+  float p = d * .3f;
+  float a = c;
+  float s = p / 4;
+  return (a * pow(2, -10 * t) * sin( (t * d - s) * (2 * PI) / p ) + c + b);
+}
+
 
 /*
  * Name:
