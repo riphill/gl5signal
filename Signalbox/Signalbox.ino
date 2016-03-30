92bbfc742a1beb21fa2d124dfc0efaab58c03f94
Richard Phillips <ccsrip@bath.ac.uk>
2016-03-30 18:08:41 +0100

    Revert "Update Signalbox.ino"
    This reverts commit 732e077d7b57e24ea45f91d7101c421ebe122afe.

--- Signalbox/Signalbox.ino
+++ Signalbox/Signalbox.ino
@@ -1,31 +1,21 @@
 /*
  * Middleton Box
- * Arduino Sketch for Arduino/Genuino Mega 2560
- * Uses Simple-Modbus Master Library
+ * Arduino Sketch for Arduino/Genuino Mega 2560 - with possible adaptions for 'boxes controlling less signals
  *
- * GL5 Mainline Association, Signal Project (for Shildon Museum display March 2016)
+ * GL5 2016
  *
  */
 // Simple Modbus Masster Library
 #include <SimpleModbusMaster.h>
 
-//////////////////// Port information ///////////////////
-#define baudrate 9600
-//#define timeout 1000
-#define timeout 500
-// #define polling 200 // the scan rate
-// knock this down real low for many bus members
-#define polling 100 // the scan rate
-#define retry_count 2
+// Modbus configurations
+#define baud 9600
+#define timeout 1000
+#define polling 200 // the scan rate
+#define retry_count 10
 
 // used to toggle the receive/transmit pin on the driver
 #define TxEnablePin 2
-
-// Map pins in array later from integers
-static const uint8_t apins[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15};
-
-// Default angle to send signals to for "OFF"
-const unsigned int DRIVEANGLE = 155;
 
 // Middleton
 enum {
@@ -57,197 +47,228 @@ enum {
   S3,
 };
 
-/*
-// Copy the relevent 'box definition to signalarms below
-
-// POST, SIGNAL NUMBER, ARM NUMBER, INPUT, ON LED pin, OFF LED pin
-const unsigned int NUMPOSTS 5;
-const unsigned int noblearms[7][6] = {
-  {1, N3, 1, 1, 34, 35},
-  {1, N6, 2, 6, 36, 37},
-  {1, N1, 3, 1, 30, 31},
-  {2, N7, 1, 7, 40, 41},
-  {3, N8, 1, 8, 42, 43},
-  {4, N5, 2, 5, 36, 37},
-  {5, N2, 1, 2, 32, 33},
+struct ARMS {
+  unsigned int postID;
+  char signalNumber[4];
 };
 
-const unsigned int NUMPOSTS = 7;
-const unsigned int middlearms[8][6] = {
-  {1, M1, 1, 1, 30, 31},
-  {2, M4, 1, 4, 36, 37},
-  {3, M2, 1, 2, 32, 33},
-  {4, M7, 1, 7, 42, 43},
-  {4, M6, 2, 6, 40, 41},
-  {5, M3, 1, 3, 34, 35},
-  {6, M8, 1, 8, 44, 45},
-  {7, M9, 1, 9, 46, 47},
+// char banana[4] = {"M1","M2","M3","M4"};
+// if MIDDLETON
+struct ARMS one = { 1, {M1, M2, M3, M4} };
+
+
+// Signal Name,
+// Post number (slave ID),
+// Input line,
+// OFF LED indicator output
+// ON  LED indicator output
+
+int noblearms[7][5] = {
+  {N3, 1, A1, 34, 35},
+  {N6, 1, A6, 36, 37},
+  {N1, 1, A1, 30, 31},
+  {N7, 2, A7, 40, 41},
+  {N8, 3, A8, 42, 43},
+  {N5, 3, A5, 36, 37},
+  {N2, 4, A2, 32, 33},
 };
 
-const unsigned int NUMPOSTS = 2;
-const unsigned int shildonarms[2][6] {
-  {1, N1, 1, 1, 30, 31},
-  {2, N2, 2, 2, 32, 33}
-};
-*/
-
-
-// POST, SIGNAL NUMBER, ARM NUMBER, INPUT (analog pin A#), ON LED, OFF LED, Drive angle?
-const unsigned int signalarms[8][6] = {
-  {1, M1, 1, 1, 30, 31},
-  {2, M4, 1, 4, 36, 37},
-  {3, M2, 1, 2, 32, 33},
-  {4, M7, 1, 7, 42, 43},
-  {4, M6, 2, 6, 40, 41},
-  {5, M3, 1, 3, 34, 35},
-  {6, M8, 1, 8, 44, 45},
-  {7, M9, 1, 9, 46, 47},
+int middlearms[8][5] = {
+  {M1, 1, A1, 30, 31},
+  {M4, 2, A4, 36, 37},
+  {M2, 3, A2, 32, 33},
+  {M7, 4, A7, 42, 43},
+  {M6, 4, A6, 40, 41},
+  {M3, 5, A3, 34, 35},
+  {M8, 6, A8, 44, 45},
+  {M9, 7, A9, 46, 47},
 };
 
-const unsigned int NUMPOSTS = 7;  // could be calculated, just not at this point
-const unsigned int NUMARMS = (sizeof(signalarms) / sizeof(int)) / 6;
+int shildonarms[2][5] {
+  {N1, 1, A1, 30, 31},
+  {N2, 2, A2, 32, 33}
+};
 
-// The total amount of available memory on the master to store data
-const int TOTAL_NO_OF_REGISTERS = (NUMARMS * 8);
-const int TOTAL_NO_OF_PACKETS = (NUMARMS * 2);
+// POST, SIGNAL NUMBER, ARM NUMBER, INPUT, ON LED, OFF LED
+int signalarms[8][6] = {
+  {1, M1, 1, A1, 30, 31},
+  {2, M4, 1, A4, 36, 37},
+  {3, M2, 1, A2, 32, 33},
+  {4, M7, 1, A7, 42, 43},
+  {4, M6, 2, A6, 40, 41},
+  {5, M3, 1, A3, 34, 35},
+  {6, M8, 1, A8, 44, 45},
+  {7, M9, 1, A9, 46, 47},
+};
+
+int numposts = 7;
+int numarms = (sizeof(signalarms) / sizeof(int)) / 6;
 
 // Create an array of Packets to be configured
-Packet packets[TOTAL_NO_OF_PACKETS];
+// NUMBER OF PACKETS is essentially the number of posts x2
+// One packet for setting the arms, the other for reading the arms
+int TOTAL_NO_OF_PACKETS = (7 * 2);
+Packet packets[(7 * 2)];
+
 
 // Masters register array
-unsigned int regs[TOTAL_NO_OF_REGISTERS];
+// The total amount of available memory on the master to store data
+// is number of posts x 2 x 4 (2 registers per arm, one read, one write, 4 per post)
+unsigned int registers[(7 * 2) * 4];
+
+static const uint8_t analog_inpins[] = {A1, A2, A3, A4, A5, A6, A7};
 
 void setup()
 {
-  int offPins[NUMARMS];  // for intialisation indications
-  int onPins[NUMARMS];
 
-#ifdef DEBUG
-  // comms with host PC
-  Serial.begin(9600);
-  // give us a chance to get the serial line open!
-  delay(2000);
-#endif
 
-  // SETUP Indicator lines
+  // setup Indicator lines
   // Signal arm ON  LED lines 31 to 51 (red, odd)
   // Signal arm OFF LED lines 30 to 50 (green, even)
-  // SETUP packets for each post
-  // read lower registers for confirmation (done in groups of 4)
-  for ( int i = 0, j = 0; i < NUMPOSTS; i++, j += 2) {
-    // INPUT lines
-    pinMode(apins[signalarms[i][3]], INPUT_PULLUP);
-    // ON indicator (red LED)
-    onPins[i] = signalarms[i][4];
-    pinMode(onPins[i], OUTPUT);
-    digitalWrite(onPins[i], HIGH);  // proving indicator, will be set later
-    // OFF indicator (green LED)
-    offPins[i] = signalarms[i][5];
-    pinMode(offPins[i], OUTPUT);
-    digitalWrite(offPins[i], HIGH);  // proving indicator, will be set later
 
-    int postid = (i + 1);
-    // set up post communication warning LEDs starting from pin 22
-    pinMode((22 + i), OUTPUT);
-    digitalWrite((22 + i), HIGH);  // proving and setting indicator, will be reset on communication
-
-    // post is counting from 0 so post 1 is 0 in this loop, no need to decrement
-    int regStart = (i * 8);
-
-    // initialise register values to 0
-    for (int k = regStart; k < (regStart+8); k++) {
-      regs[k] = 0;
+  for ( int i = 0 ; i < numarms; i++ ) {
+    for ( int j = 0 ; i < 5 ; i++ ) {
+      switch (j) {
+        case 0:
+          // post IDs
+          break;
+        case 1:
+          // Signal Number
+          break;
+        case 2:
+          // Arm Number
+          break;
+        case 3:
+          // input lines
+          pinMode(signalarms[i][j], INPUT_PULLUP);
+          break;
+        case 5:
+          // OFF indicator
+          pinMode(signalarms[i][j], OUTPUT);
+          break;
+        case 6:
+          // ON indicator (green LED)
+          pinMode(signalarms[i][j], OUTPUT);
+          break;
+      }
     }
-    
-    // read registers
-    modbus_construct(&packets[j], postid, READ_HOLDING_REGISTERS, 0, 4, regStart);
-    // set upper registers
-    modbus_construct(&packets[j + 1], postid, PRESET_MULTIPLE_REGISTERS, 4, 4, (regStart + 4));
   }
 
-  // After setup set all indicator lines HIGH, then to typical positions.
-  // put a delay in so LED lines show on for a bit
-  delay(500);
+  // setup packets for each post
+  // read lower registers for confirmation (done in groups of 4)
+  for ( int postid = 0, i = 0; postid < numposts; postid++, i += 2) {
+    // set up post communication warning LEDs starting from pin 22
+    pinMode((22 + i), OUTPUT);
 
-  modbus_configure(&Serial, baudrate, SERIAL_8N2, timeout, polling, retry_count, TxEnablePin, packets, TOTAL_NO_OF_PACKETS, regs);
+    int startreg = (postid - 1) * 8;
+    modbus_construct(&packets[i], postid, READ_HOLDING_REGISTERS, 0, 3, startreg);
+    // set upper registers
+    modbus_construct(&packets[i + 1], postid, PRESET_MULTIPLE_REGISTERS, 4, 7, startreg + 4);
+  }
+  // comms with host PC
+  Serial.begin(9600);
+
+  // Initialize the Modbus Finite State Machine
+  modbus_configure(&Serial1, baud, SERIAL_8N2, timeout, polling, retry_count, TxEnablePin, packets, TOTAL_NO_OF_PACKETS, registers);
+
+  // After setup set all indicator lines HIGH, then to typical positions.
+
 }
 
 void loop()
 {
   modbus_update();
-  int incycle = 1;
+  /*
+   Serial.print("requests: ");
+   Serial.println(packets[POST2SET].requests);
+   Serial.print("successful_requests: ");
+   Serial.println(packets[POST2SET].successful_requests);
+   Serial.print("failed_requests: ");
+   Serial.println(packets[POST2SET].failed_requests);
+   Serial.print("exception_errors: ");
+   Serial.println(packets[POST2SET].exception_errors);
+   */
 
-  //  if (incycle == 1) {
+  Serial.print("retries: ");
+  Serial.println(packets[0].retries);
+  Serial.print("connection: ");
+  Serial.println(packets[0].connection);
+  Serial.println("------------------------------");
+  /*
+    Serial.print("retries 2: ");
+    Serial.println(packets[1].retries);
+    Serial.print("connection 2: ");
+    Serial.println(packets[1].connection);
+    Serial.println("------------------------------");
+
+    Serial.print("regs 0 : ");
+    Serial.println(regs[0]);
+    Serial.println(regs[1]);
+    Serial.println(regs[2]);
+  /*
+    Serial.print("Switch 1 regs: ");
+    Serial.print(regs[0]);
+    Serial.print(" val: ");
+    Serial.println(val1);
+    Serial.print("Switch 2 regs: ");
+    Serial.print(regs[1]);
+    Serial.print(" val: ");
+    Serial.println(val2);
+    Serial.print("Switch 3 regs: ");
+    Serial.print(regs[2]);
+    Serial.print(" val: ");
+    Serial.println(val3);
+    */
+  Serial.println("--------");
+
+  // Set post Comms state, check packet, check only the read register packet per post
   int commsstate = 0;
-  for ( int h = 0, i = 0; h < NUMPOSTS; h++, i += 2) {
-    commsstate = packets[i].connection;
-#ifdef DEBUG
-    Serial.print("Packet : ");
-    Serial.print(i);
-    Serial.print(", comms state: ");
-    Serial.println(commsstate);
-#endif
-    digitalWrite((22 + h), (commsstate ? LOW : HIGH));
+  for ( int postid = 1, i = 0; postid <= numposts; postid++, i += 2) {
+    commsstate = packets[(postid * 2) - 1].connection;
+    //TODO: might have to invert the state here
+    digitalWrite((22 + postid), commsstate);
+    /*
+        int startreg = (postid - 1) * 8;
+        // read the four registers from the post
+        for (int regcount = 0; regcount < 4; regcount++) {
+          int armposval = 0;
+          armposval = registers[(startreg + regcount)];
+
+          // work out which arm this applies to.
+        }
+    */
   }
-  incycle = 2;
-  // }
 
-  if (incycle == 2) {
-    // POST 1 read registers   0 -> 3
-    // POST 1 WRITE registers  4 -> 7
-    // POST 2 read registers   8 -> 11
-    // POST 2 WRITE registers 12 -> 15
-    // POST 3 read registers  16 -> 19
-    // POST 3 WRITE registers 20 -> 23
-    // POST 4 read registers  24 -> 27
-    // POST 4 WRITE registers 28 -> 31
 
-    // this means going through the ARMS eg A1 to A7
-    int sPinState = LOW;
-    int sArmState = 0;  // arm state ON (0) or OFF (>0)
-    int armidx = NUMARMS;
-    while (armidx) {
-      --armidx;
-      // read input lines A0 to A7...
-      sPinState = digitalRead(apins[signalarms[armidx][3]]);
+  // Since we don't have enough interrupt pins, check the state of the INPUT pins
+  // loop through arm array, check input value for that arm, set state based upon it.
 
-      int postIDtoUpdate = signalarms[armidx][0];
-      int regOffset = signalarms[armidx][2] - 1 ;  // ie the Arm on the post
-      int regStart = ((postIDtoUpdate - 1) * 8);
+  // this means going through the ARMS eg A1 to A7
+  int sPinState = LOW;
+  int sArmState = 0;  // arm state ON (0) or OFF (>0)
+  int armidx = numarms;
+  while (armidx) {
+    --armidx;
+    // read input lines A0 to A7...
+    sPinState = digitalRead(signalarms[armidx][3]);
 
-      // GETTING (first set of 4 registers)
-      sArmState = regs[(regStart + regOffset)];
+    int postIDtoUpdate = signalarms[armidx][0];
+    int regtoUpdate = signalarms[armidx][2];
 
-#ifdef DEBUG
-      Serial.print("Arm ID: ");
-      Serial.print(postIDtoUpdate);
-      Serial.print(", Label: ");
-      Serial.print(signalarms[armidx][1]);
-      Serial.print(", Arm #: ");
-      Serial.print(signalarms[armidx][2]);
-      Serial.print(", Input #: ");
-      Serial.print(signalarms[armidx][3]);
-      Serial.print("input status: ");
-      Serial.print(sPinState);
-      Serial.print(" Resgister Start: ");
-      Serial.print(regStart);
-      Serial.print(" Register Offset: ");
-      Serial.print(regOffset);
-      Serial.print(" Arm State: ");
-      Serial.print(sArmState);
-      Serial.print(" Set Reg: ");
-      Serial.println(registers[((regStart + 4) + regOffset)]);
-      Serial.println("------------------------------");
-#endif
+    // GETTING
+    sArmState = registers[((postIDtoUpdate - 1) * 8) + regtoUpdate];
+    digitalWrite(signalarms[armidx][5], ((sArmState) ? : HIGH, LOW));  // ON LED (red)
+    digitalWrite(signalarms[armidx][6], ((!sArmState) ? : HIGH, LOW)); // OFF LED (green)
 
-      digitalWrite(signalarms[armidx][4], ((sArmState == 0 )? HIGH : LOW));  // ON LED (red)
-      digitalWrite(signalarms[armidx][5], ((sArmState  > 0) ? HIGH : LOW)); // OFF LED (green)
-
-      // SETTING (next set of 4 registers)
-      // if pin grounded (inverted on INPUT_PULLUP), set the relevant register
-      regs[((regStart + 4) + regOffset)] = ((sPinState == HIGH) ? 0 : 155); // degree to drive to
-    }
-
-    incycle = 1;
+    // SETTING
+    // if pin grounded, set the relevant register
+    registers[((postIDtoUpdate - 1) * 8) + 4 + regtoUpdate] = (sPinState == LOW) ? 0 : 155; // degree to drive to
   }
 }
+
+/*
+// when given post ID and an arm register, return the output pin indicator pairs?
+void function getArmIndex(int &pinpair[], int pinpairsize, int postid) {
+
+}
+*/
